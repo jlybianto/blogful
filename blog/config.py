d308642c6f34056fcfe769cea0d1262f723550ab
jlybianto <jesse.lybianto@gmail.com>
2015-05-21 02:01:53 +0000

    Added TestingConfig class

--- blog/config.py
+++ blog/config.py
@@ -1,9 +1,16 @@
 import os
 
+# To contain the configuration variables which control the Flask app.
+# SQLALCHEMY_DATABASE_URI to set location of database, 
+# DEBUG mode to help track down any errors in application,
+# and SECRET_KEY to secure application.
+  
 class DevelopmentConfig(object):
-  """
-  To contain the configuration variables which control the Flask app. SQLALCHEMY_DATABASE_URI to set location of database, DEBUG mode to help track down any errors in application and SECRET_KEY to secure application.
-  """
   SQLALCHEMY_DATABASE_URI = "postgresql://action:action@localhost:5432/blogful"
   DEBUG = True
   SECRET_KEY = os.environ.get("BLOGFUL_SECRET_KEY", "PETER_PARKER_IS_SPIDERMAN")
+
+class TestingConfig(object):
+  SQLALCHEMY_DATABASE_URI = "postgresql://action:action@localhost:5432/blogful-test"
+  DEBUG = False
+  SECRET_KEY = "Not secret"
