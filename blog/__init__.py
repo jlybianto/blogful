63227645a165782e5b827cd21daf55ffc61cf0e5
jlybianto <jesse.lybianto@gmail.com>
2015-05-22 05:12:24 +0000

    Change environment to TravisConfig

--- blog/__init__.py
+++ blog/__init__.py
@@ -6,7 +6,7 @@ from flask import Flask
 app = Flask(__name__)
 
 # Use config_path to switch between configurations easily in different situations
-config_path = os.environ.get("CONFIG_PATH", "blog.config.DevelopmentConfig")
+config_path = os.environ.get("CONFIG_PATH", "blog.config.TravisConfig")
 app.config.from_object(config_path)
 
 # Import Views and Jinja filters after app creation because both make use of app object
